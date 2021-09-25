fa5e264b23d9e0d807135875d8dd3b02fa446cce
andre.gustavo <andre.sanchessilva@gmail.com>
2021-09-25 12:49:51 -0300

    tags 🏷

---
+++ src/models/edicao.ts
@@ -0,0 +1,41 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Application } from '../declarations';
+import { HookReturn } from 'sequelize/types/lib/hooks';
+
+export default function (app: Application): typeof Model {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const edicao = sequelizeClient.define('edicao', {
+    edicao: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ano: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    eventoId: {
+      type: DataTypes.INTEGER,
+    },
+    instituicaoId: {
+      type: DataTypes.INTEGER,
+    },
+  }, {
+    hooks: {
+      beforeCount(options: any): HookReturn {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (edicao as any).associate = function (models: typeof sequelizeClient.models): void {
+
+    edicao.belongsTo(models.instituicao, { as: 'instituicao', foreignKey: 'instituicaoId' });
+    edicao.belongsTo(models.evento, { as: 'evento', foreignKey: 'eventoId' });
+
+  };
+
+  return edicao;
+}
