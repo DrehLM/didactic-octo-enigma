d5e79b44f29902d4ec21a44a1a6c530a182f9995
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-09-25 12:37:54 -0300

    adiciona service de publicacoes

--- src/services/index.ts
+++ src/services/index.ts
@@ -7,6 +7,7 @@ import orientadores from './orientadores/orientadores.service';
 import edicoes from './edicoes/edicoes.service';
 import trilhas from './trilhas/trilhas.service';
 import edicoesTrilhas from './edicoes-trilhas/edicoes-trilhas.service';
+import publicacoes from './publicacoes/publicacoes.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
@@ -18,4 +19,5 @@ export default function (app: Application): void {
   app.configure(edicoes);
   app.configure(trilhas);
   app.configure(edicoesTrilhas);
+  app.configure(publicacoes);
 }
