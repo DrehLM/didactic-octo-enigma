db3fd2b0d239056fc6c9c19efc269ea268f9c26c
andre.gustavo <andre.gustavo@eurekalabs.com.br>
2021-09-25 01:42:13 -0300

    instituicoes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import { Application } from '../declarations';
 import eventos from './eventos/eventos.service';
+import instituicoes from './instituicoes/instituicoes.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(eventos);
+  app.configure(instituicoes);
 }
