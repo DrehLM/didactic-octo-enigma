70eafc3d73774e975cb2cb5f8d70518f41edc127
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-09-26 17:28:07 -0300

    inclui associacoes das publicacoes

---
+++ backend/src/hooks/publicacoes/include-associations.ts
@@ -0,0 +1,25 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function includeAssociations(): Hook {
+  return async (context) => {
+    const { models } = context.app.get('sequelizeClient');
+
+    context.params.sequelize = {
+      ...(context.params.sequelize ?? {}),
+      raw: false,
+      include: [
+        ...(context.params.sequelize?.include ?? []),
+        { model: models.edicao, as: 'edicao' },
+        { model: models.trilha, as: 'trilha' },
+        { model: models.tag, as: 'tags' },
+        { model: models.escrita, as: 'escritas' },
+        { model: models.palavraChave, as: 'palavrasChaves' },
+      ],
+    };
+
+    return context;
+  };
+}
