2593e1a0da97e36a82cce671cff9134eae759920
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-09-26 01:56:44 -0300

    adiciona logo e titulo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import {
   AppBar,
   Box,
   CssBaseline,
+  Grid,
   IconButton,
   ThemeProvider,
-  Toolbar
+  Toolbar,
+  Typography
 } from '@mui/material';
 import React from 'react';
+import logo from './assets/logo512.png';
 import { mainTheme } from './styles/MainTheme';
 
 function App() {
@@ -26,6 +29,16 @@ function App() {
             >
               <MenuIcon />
             </IconButton>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item>
+                <img src={logo} alt="Octoper Logo" height="40vh" />
+              </Grid>
+              <Grid item>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Octoper
+                </Typography>
+              </Grid>
+            </Grid>
           </Toolbar>
         </AppBar>
       </Box>
