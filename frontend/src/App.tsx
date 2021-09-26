09d0a058a1bec35d524d3d1208eeb8335d0a7ec5
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-09-26 01:23:26 -0300

    adiciona AppBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import { CssBaseline } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, CssBaseline, IconButton, Toolbar } from '@mui/material';
 import React from 'react';
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </>
   );
 }
