d8d3aebe79c808bec63ca06e0743e20568557862
andre.gustavo <andre.sanchessilva@gmail.com>
2021-09-26 14:10:16 -0300

    icons 💅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
-import { Person as PersonIcon } from '@mui/icons-material';
+import PersonIcon from '@mui/icons-material/Person';
 import SchoolIcon from '@mui/icons-material/School';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import EventIcon from '@mui/icons-material/Event';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import DescriptionIcon from '@mui/icons-material/Description';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
@@ -11,14 +16,39 @@ import { mainTheme } from './styles/MainTheme';
 
 const menuItems: MenuItem[] = [
   {
-    label: 'Autores',
-    path: '/autores',
-    icon: PersonIcon,
+    label: 'Eventos',
+    path: '/eventos',
+    icon: PushPinIcon,
+  },
+  {
+    label: 'Edições',
+    path: '/edicoes',
+    icon: EventIcon,
   },
   {
     label: 'Instituições',
     path: '/instituicoes',
     icon: SchoolIcon,
+  },
+  {
+    label: 'Autores',
+    path: '/autores',
+    icon: PersonIcon,
+  },
+  {
+    label: 'Publicações',
+    path: '/publicacoes',
+    icon: DescriptionIcon,
+  },
+  {
+    label: 'Tags',
+    path: '/tags',
+    icon: LocalOfferIcon,
+  },
+  {
+    label: 'Palavras-chave',
+    path: '/palavras-chave',
+    icon: VpnKeyIcon,
   },
 ];
 
