27f9f03ed0a5336c33e58b1f19332587c497e944
Mateus Fuga Osmarin <mateus.osmarin@gmail.com>
2021-09-26 03:15:13 -0300

    adiciona RFFTextField

--- frontend/src/components/finalFormFields/RFFTextField.tsx
+++ frontend/src/components/finalFormFields/RFFTextField.tsx
@@ -1,6 +1,58 @@
-import { Field } from 'react-final-form';
-import { TextField, TextFieldProps } from '@mui/material';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
+import React, { FocusEvent } from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import Field from './Field';
 
-const RFFTextField = ({ name, ...rest }: TextFieldProps) => {
-  <Field name={name}>{(props) => <TextField name={name} />}</Field>;
+export type RFFTextFieldProps = TextFieldProps & {
+  name: string;
+  label: string;
+  fullWidth: boolean;
+  placeholder?: string;
 };
+
+const handleOnBlur = (
+  event: FocusEvent<HTMLInputElement | HTMLTextAreaElement>,
+  callbacks: (Function | undefined)[]
+) => {
+  for (const callback of callbacks) {
+    if (callback) {
+      callback(event);
+    }
+  }
+};
+
+const RFFTextField = ({
+  fullWidth,
+  label,
+  name,
+  ...others
+}: RFFTextFieldProps) => (
+  <Field name={name}>
+    {({
+      input,
+      meta,
+      ...props
+    }: FieldRenderProps<string, HTMLInputElement | HTMLTextAreaElement>) => (
+      <TextField
+        {...input}
+        {...props}
+        {...others}
+        fullWidth={fullWidth}
+        label={label}
+        error={meta.touched && !!meta.error}
+        helperText={meta.touched && meta.error}
+        onBlur={(event: FocusEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+          handleOnBlur(event, [input.onBlur, others.onBlur])
+        }
+      />
+    )}
+  </Field>
+);
+
+RFFTextField.defaultProps = {
+  fullWidth: false,
+  endAdornment: 'disabled',
+  label: ''
+};
+
+export default RFFTextField;
