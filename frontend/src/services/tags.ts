016b81f1d3f416c1e43deb05f8c7f73d56e42981
andre.gustavo <andre.sanchessilva@gmail.com>
2021-09-26 05:37:47 -0300

    tags api 🏷🔥

--- frontend/src/services/tags.ts
+++ frontend/src/services/tags.ts
@@ -1,8 +1,38 @@
 import api from './api';
 
 const tags = {
-  get: async () => {
-    const response = await api.get<Paginated<Tag>>('/tags');
+  find: async (tag: Partial<Tag>) => {
+    const response = await api.get<Paginated<Tag>>('/tags', tag);
+
+    if (response.ok) {
+      return response.data!;
+    } else {
+      throw new Error(response.originalError.message);
+    }
+  },
+
+  create: async (tag: Tag) => {
+    const response = await api.post('/tags', tag);
+
+    if (response.ok) {
+      return response.data!;
+    } else {
+      throw new Error(response.originalError.message);
+    }
+  },
+
+  remove: async (id: number) => {
+    const response = await api.delete(`/tags/${id}`);
+
+    if (response.ok) {
+      return response.data!;
+    } else {
+      throw new Error(response.originalError.message);
+    }
+  },
+
+  update: async (tag: Tag) => {
+    const response = await api.delete(`/tags/${tag.id}`, tag);
 
     if (response.ok) {
       return response.data!;
