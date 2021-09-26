1656091a0db76a108c4dda796d6220272aca7f0d
andre.gustavo <andre.sanchessilva@gmail.com>
2021-09-26 00:01:40 -0300

    prettier 💅

--- frontend/src/styles/MainTheme.ts
+++ frontend/src/styles/MainTheme.ts
@@ -2,34 +2,34 @@ import { createTheme, ThemeOptions } from '@mui/material';
 import { ptBR } from '@mui/material/locale';
 
 const baseTheme: ThemeOptions = {
-    palette: {
-        primary: {
-            main: '#ff5c5f',
-        },
-        secondary: {
-            main: '#99e1d9',
-        },
-        background: {
-            default: '#fcfcfc',
-            paper: '#fdfdfd',
-        },
-        text: {
-            primary: '#454545',
-            secondary: '#525252',
-        },
-        success: {
-            main: '#a6ce38'
-        },
-        error: {
-            main: '#ff1f22'
-        },
+  palette: {
+    primary: {
+      main: '#ff5c5f',
     },
-}
+    secondary: {
+      main: '#99e1d9',
+    },
+    background: {
+      default: '#fcfcfc',
+      paper: '#fdfdfd',
+    },
+    text: {
+      primary: '#454545',
+      secondary: '#525252',
+    },
+    success: {
+      main: '#a6ce38',
+    },
+    error: {
+      main: '#ff1f22',
+    },
+  },
+};
 
 const buildTheme = (themeOptions: ThemeOptions) => {
-    const newTheme = { ...themeOptions };
-    newTheme.palette = newTheme.palette || {};
-    return createTheme(themeOptions, ptBR);
+  const newTheme = { ...themeOptions };
+  newTheme.palette = newTheme.palette || {};
+  return createTheme(themeOptions, ptBR);
 };
 
 export const mainTheme = buildTheme(baseTheme);
